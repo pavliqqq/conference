8b2c950e2a43a905c839c5a13b8b4b0db3fe67d2
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-06-14 18:21:00 +0300

    Edit all_members.blade.php

--- app/Views/all_members.blade.php
+++ app/Views/all_members.blade.php
@@ -5,13 +5,13 @@
     <div class="max-w-4xl mx-auto mt-10 p-6 bg-white shadow rounded">
         <h2 class="text-xl text-center font-semibold mb-4">All Members</h2>
 
-        <table class="w-full border border-gray-300 rounded overflow-hidden">
+        <table class="w-full border border-gray-300 rounded overflow-hidden table-fixed">
             <thead class="bg-gray-100">
             <tr>
-                <th class="p-3 text-left border-b">Photo</th>
-                <th class="p-3 text-left border-b">Full Name</th>
-                <th class="p-3 text-left border-b">Report Subject</th>
-                <th class="p-3 text-left border-b">Email</th>
+                <th class="p-3 text-left border-b w-[80px]">Photo</th>
+                <th class="p-3 text-left border-b w-[200px] truncate">Full Name</th>
+                <th class="p-3 text-left border-b w-[300px] truncate">Report Subject</th>
+                <th class="p-3 text-left border-b w-[250px] truncate">Email</th>
             </tr>
             </thead>
             <tbody id="members-table-body">
@@ -21,10 +21,11 @@
                         <img src="{{ $member['photo'] }}" alt="{{ $member['full_name'] }}"
                              class="h-12 w-12 object-cover rounded-full">
                     </td>
-                    <td class="p-3">{{ $member['full_name'] }}</td>
-                    <td class="p-3">{{ $member['report_subject'] }}</td>
-                    <td class="p-3">
-                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to={{ $member['email'] }}" target="_blank" class="text-blue-600 hover:underline">
+                    <td class="p-3 break-words">{{ $member['full_name'] }}</td>
+                    <td class="p-3 break-words">{{ $member['report_subject'] }}</td>
+                    <td class="p-3 break-words">
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to={{ $member['email'] }}"
+                           target="_blank" class="text-blue-600 hover:underline">
                             {{ $member['email'] }}
                         </a>
                     </td>
