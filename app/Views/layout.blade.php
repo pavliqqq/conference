cd467e27a3d7d640607f33a8a27dbe95c4b44599
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-06-17 20:57:05 +0300

    Edit layout.blade.php

--- app/Views/layout.blade.php
+++ app/Views/layout.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Wizard Form</title>
+    @stack('head')
     <script src="https://cdn.tailwindcss.com"></script>
-    @yield('head')
 </head>
 <body class="bg-gray-100">
-    <div>
-            @yield('content')
-    </div>
+<div>
+    @yield('content')
+</div>
+@stack('scripts')
 </body>
 </html>
